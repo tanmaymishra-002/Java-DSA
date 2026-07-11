ceec3bc48049b28485e2f0fe51a2a7ff2c644743
Tanmay Mishra <tanmaymishra.129@gmail.com>
2026-07-11 18:56:32 +0530

    Arrays

---
+++ ARRAYS/FRUITS.java
@@ -0,0 +1,25 @@
+
+import java.util.HashMap;
+
+class FRUITS{
+    public static void main(String[] args) {
+         int [] fruits = {1, 2, 1};
+         int left =0;
+         int right =0;
+         int maxlength=0;
+         HashMap <Integer,Integer> map = new HashMap<>();
+         while(right<fruits.length){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size()>2){
+                map.put(fruits[right],map.get(fruits[left]-1));
+                if(map.get(fruits[left])==0){
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+            maxlength = Math.max(maxlength,right-left+1);
+            right++;
+         }
+         System.out.println(maxlength);
+    }
+}
