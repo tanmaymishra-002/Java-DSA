c6340c66ad7463d0b0d1a160ab00ee49efa527ba
Tanmay Mishra <tanmaymishra.129@gmail.com>
2026-07-11 06:13:21 +0530

    Binary search

---
+++ BINARY SEARCH/searchinrotated.java
@@ -0,0 +1,44 @@
+class searchinrotated {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        searchinrotated obj = new searchinrotated();
+
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        int ans = obj.search(nums, target);
+
+        System.out.println("Target found at index: " + ans);
+    }
+}
