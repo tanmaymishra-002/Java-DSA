cbe398df8e50b2285a7b2315faba2dec23708b7b
Tanmay Mishra <tanmaymishra.129@gmail.com>
2026-07-17 07:32:19 +0530

    Stack

--- STACKS & QUEUE/NextSmallerElement.java
+++ STACKS & QUEUE/NextSmallerElement.java
@@ -1,36 +1,26 @@
 import java.util.*;
-
 class NextSmallerElement {
-
     public int[] nextSmallerElements(int[] arr) {
         int n = arr.length;
         Stack<Integer> st = new Stack<>();
         int[] res = new int[n];
-
         for (int i = n - 1; i >= 0; i--) {
             while (!st.isEmpty() && st.peek() >= arr[i]) {
                 st.pop();
             }
-
             if (st.isEmpty()) {
                 res[i] = -1;
             } else {
                 res[i] = st.peek();
             }
-
             st.push(arr[i]);
         }
-
         return res;
     }
-
     public static void main(String[] args) {
         NextSmallerElement obj = new NextSmallerElement();
-
         int[] arr = {4, 8, 5, 2, 25};
-
         int[] ans = obj.nextSmallerElements(arr);
-
         System.out.println(Arrays.toString(ans));
     }
 }
---
+++ STACKS & QUEUE/TrappingRainwater.java
@@ -0,0 +1,30 @@
+// NOT EXACTLY A STACK PROBLEM
+class TrappingRainwater{
+    public static void main(String[] args) {
+        int [] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int left =0;
+        int right =height.length-1;
+        int leftmax = 0;
+        int rightmax =0;
+        int total =0;
+
+        while(left<right){
+            if(height[left]<=height[right]){
+                if(leftmax>height[left]){
+                    total += leftmax - height[left];
+                }else{
+                    leftmax = height[left];
+                }
+                left++;
+            }else{
+                if(rightmax>height[right]){
+                    total+=rightmax-height[right];
+                }else{
+                    rightmax = height[right];
+                }
+                right--;
+            }
+        }
+        System.out.println(total);
+    }
+}
