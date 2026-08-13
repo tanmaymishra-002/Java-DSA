a3cea9bb831038dd05b8966520c4a7787c9e20d2
Tanmay Mishra <tanmaymishra.129@gmail.com>
2026-08-13 07:44:26 +0530

    Greedy

---
+++ Greedy/LemonadeChange.java
@@ -0,0 +1,38 @@
+public class LemonadeChange {
+    public static boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            }
+            else if (bill == 10) {
+                if (five > 0) {
+                    five--;
+                    ten++;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] bills = {5, 5, 5, 10, 20};
+        System.out.println(lemonadeChange(bills));
+    }
+}
