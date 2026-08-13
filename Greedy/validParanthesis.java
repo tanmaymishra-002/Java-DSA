f52c6ea5b930440f79eac7c4cc90b5bcb21987e7
Tanmay Mishra <tanmaymishra.129@gmail.com>
2026-08-13 08:18:47 +0530

    Greedy

---
+++ Greedy/validParanthesis.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class validParanthesis {
+    public static boolean checkValidString(String s) {
+        int min = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                min++;
+                max++;
+            } else if (ch == ')') {
+                min--;
+                max--;
+            } else {
+                min--;
+                max++;
+            }
+            if (min < 0) {
+                min = 0;
+            }
+            if (max < 0) {
+                return false;
+            }
+        }
+        return min == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(checkValidString(s));
+
+        sc.close();
+    }
+}
